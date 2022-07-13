76980ee98a69971b95a93ac3570b915a6a81fdbb
bombi <bombic3@naver.com>
2022-07-13 13:45:30 +0900

    FEAT : 색상 컴포넌트 SelectColors 색상 변경 기능 구현

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,22 +1,33 @@
+import { ColorConsumer } from "../contexts/color";
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const SelectColors = () => {
   return (
     <div>
       <h2>색상을 선택하세요.</h2>
-      <div style={{ display: 'flex' }}>
-        {colors.map(color => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: '24px',
-              height: '24px',
-              cursor: 'pointer'
-            }}
-          />
-        ))}
-      </div>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: 'flex' }}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer'
+              }}
+              onClick={() => actions.setColor(color)}
+              onContextMenu={e => {
+                e.preventDefault(); // 마우스 우클락 시 메뉴 뜨는 것 방지
+                actions.setSubcolor(color);
+              }}
+            />
+          ))}
+          </div>
+        )}  
+      </ColorConsumer>
       <hr />
     </div>
   );
