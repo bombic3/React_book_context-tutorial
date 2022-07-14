1fab4688e820ed50f410819a0608eb78bf22f3fc
bombi <bombic3@naver.com>
2022-07-14 10:01:04 +0900

    REFACTOR : 클래스형 컴포넌트에서 static contextType을 정의하여 Consumer대신 기능 구현하기

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,7 +1,49 @@
-import { ColorConsumer } from "../contexts/color";
+import { Component } from "react";
+import ColorContext from "../contexts/color";
+// 클래스형 컴포넌트에서 Context 쉽게 사용하기
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+class SelectColors extends Component {
+  // static contextType 정의
+  static contextType = ColorContext;
+  // 만약 setColor를 호출하고 싶다면 this.context.actions.setColor를 호출하면 됨
+  handleSetColor = color => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetSubcolor = subcolor => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+
+  render () {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: 'flex' }}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: "pointer"
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={e => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+/*
 const SelectColors = () => {
   return (
     <div>
@@ -32,5 +74,5 @@ const SelectColors = () => {
     </div>
   );
 };
-
+*/
 export default SelectColors;
